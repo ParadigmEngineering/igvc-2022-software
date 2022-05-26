0fe903e112fe431e0ea9f27fb2cc3289f4495f6b
Daniel Burke <danielseanburke@gmail.com>
2022-05-26 13:05:41 -0230

    Add header of CAN message IDs

---
+++ Node/Core/Inc/can_message_defs.h
@@ -0,0 +1,20 @@
+#define WIRELESS_ESTOP  120
+#define MOTOR_CONTROL_1 130
+#define MOTOR_CONTROL_2 131
+#define MOTOR_CONTROL_3 132
+
+#define LED_ON_TEST_MASK 0x0100
+#define LED_1_TEST_ON    0x0101
+#define LED_2_TEST_ON    0x0102
+#define LAMP_1_TEST_ON   0x0103
+#define LAMP_2_TEST_ON   0x0104
+#define LAMP_3_TEST_ON   0x0105
+#define LAMP_4_TEST_ON   0x0106
+
+#define LED_OFF_TEST_MASK 0x0200
+#define LED_1_TEST_OFF    0x0201
+#define LED_2_TEST_OFF    0x0202
+#define LAMP_1_TEST_OFF   0x0203
+#define LAMP_2_TEST_OFF   0x0204
+#define LAMP_3_TEST_OFF   0x0205
+#define LAMP_4_TEST_OFF   0x0206
