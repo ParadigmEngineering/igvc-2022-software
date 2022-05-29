7c12ee0d48aa5f634707f95e31572b95e0e2c3f0
Colton Smith <cnsmith@mun.ca>
2022-05-29 16:28:41 -0230

    Changer para-control pkg to para_control

--- ROS/para_control/scripts/open_base_teleop.py
+++ ROS/para_control/scripts/open_base_teleop.py
@@ -5,14 +5,17 @@ Listen for key presses and convert to open_base commands.
 
 import rospy
 import sys, termios, tty
+from open_base.msg import Movement, MovementGeneric, Velocity
 
 
 move_bindings = \
 {
-	"W": (0, 1),
-	"A": (-1, 0),
-	"S": (0, -1),
-	"D": (1, 0),
+	'w': [0, 1],
+	'a': [-1, 0],
+	's': [0, -1],
+	'd': [1, 0],
+    'q': [-1, 1],
+    'e': [1, 1]
 }
 
 
@@ -33,9 +36,38 @@ def get_key():
 
 def main():
     rospy.init_node("open_base_teleop", anonymous=True, log_level=rospy.INFO)
-    rospy.loginfo("Starting open base keyobaord teleop node...")
+    rospy.loginfo("Starting open base keyboard teleop node...")
+
+    max_speed = 1
+    vel = [0, 0]
+
+    while (1):
+        key = get_key()
+
+        if key in move_bindings.keys():
+            vel = move_bindings[key]
+
+        elif key in speed_bindings.keys():
+            max_speed += speed_bindings[key]
+
+        else: 
+            vel = [0, 0]
+            if (key == '\x03'):
+                rospy.loginfo("Keyboard interrupt! Exiting...")
+                break
+
+        vel[0] = vel[0] * max_speed
+        vel[1] = vel[1] * max_speed
+
+        rospy.loginfo(f"VEL_X: {vel[0]} | VEL_Y: {vel[1]}")
+
+        # TODO: Publish velocity command in open_base format
 
 
 if __name__ == "__main__":
     settings = termios.tcgetattr(sys.stdin)
-    main()
+
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
