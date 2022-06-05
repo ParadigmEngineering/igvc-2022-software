030d9476d5a36a39d2399549fc4a4f632fcb3543
Andrew Nash <anash@mun.ca>
2022-06-04 23:47:10 -0230

    Integrate ML models with ROS

--- ROS/para_ai/scripts/zed_to_seg.py
+++ ROS/para_ai/scripts/zed_to_seg.py
@@ -3,8 +3,8 @@
 Listen for zed images and segformer.
 """
 
-import cv2
 import cv_bridge
+import os
 import numpy as np
 import rospy
 
@@ -14,7 +14,6 @@ from torch import int8, nn
 from transformers import SegformerForSemanticSegmentation, SegformerFeatureExtractor
 
 
-# Load model to gpu 
 class Model():
     palette = np.array([
         [ 81,   0,  81], 
@@ -25,10 +24,10 @@ class Model():
         [230, 150, 140],
         [ 70,  70,  70]])
     
-    def __init__(self, model_path: str, gpu_id: int, topic_out: str):
+    def __init__(self, model_path, gpu_id=0, topic_out="images/seg"):
         self.feature_extractor = SegformerFeatureExtractor(reduce_labels=True)
         self.model = SegformerForSemanticSegmentation.from_pretrained(model_path)
-        self.publisher = rospy.Publisher(topic_out, Image)
+        self.publisher = rospy.Publisher(topic_out, Image, queue_size=10)
         self.device = f"cuda:{gpu_id}"
         self.bridge = cv_bridge.CvBridge() 
         self.model.to(self.device)
@@ -36,27 +35,23 @@ class Model():
     def predict(self, img_msg: Image):
         """ Feed an image message to the seg model, publish the output """
         cv_image = self.bridge.imgmsg_to_cv2(img_msg, desired_encoding='rgb8')
-        rospy.loginfo(cv_image.shape)
         encoding = self.feature_extractor(cv_image, return_tensors="pt")
-
         pixel_values = encoding.pixel_values.to(self.device)
-        print(pixel_values.shape)
-    
         outputs = self.model(pixel_values=pixel_values)
         logits = outputs.logits.cpu()
         upsampled_logits = nn.functional.interpolate(logits,
                         size=(256, 512),
                         mode='bilinear',
                         align_corners=False)
-        pred_seg_img = upsampled_logits.argmax(dim=1)[0]
+        pred_img_encoded = upsampled_logits.argmax(dim=1)[0]
 
-        color_seg = np.zeros((256, 512, 3), dtype=np.uint8)
+        pred_img = np.zeros((256, 512, 3), dtype=np.uint8)
 
         for label, color in enumerate(self.palette):
-            color_seg[pred_seg_img == label, :] = color
+            pred_img[pred_img_encoded == label, :] = color
         
-        color_seg = cvtColor(color_seg, cv2.COLOR_BGR2RGB)
-        segmented_img_msg = self.bridge.cv2_to_imgmsg(color_seg, encoding="passthrough")
+        pred_img = cvtColor(pred_img, COLOR_BGR2RGB)
+        segmented_img_msg = self.bridge.cv2_to_imgmsg(pred_img, encoding="passthrough")
         self.publisher.publish(segmented_img_msg)
 
         
@@ -67,10 +62,11 @@ def main():
     gpu_id = rospy.get_param("~gpu_id")
     seg_image_pub_topic = rospy.get_param("~seg_image_out_topic")
     zed_topic = rospy.get_param("~zed_rgb_rectified_topic")
-    model_path = rospy.get_param("~model_path")
+    model_folder_path = rospy.get_param("model_folder_path")
+    model_name = rospy.get_param("~model_name")
 
     rospy.loginfo(f"Loading seg ML model to GPU {gpu_id}...")
-    model = Model(model_path, gpu_id, seg_image_pub_topic)
+    model = Model(os.path.join(model_folder_path, model_name), gpu_id, seg_image_pub_topic)
 
     rospy.loginfo("Waiting for zed images...")
     rospy.Subscriber(zed_topic, Image, model.predict)
